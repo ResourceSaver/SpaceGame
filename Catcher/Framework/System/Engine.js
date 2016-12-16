1a4fcb544367da810dce91fd1c29cfebcf873fd7
ResourceSaver <masterproject30@gmail.com>
2016-12-16 14:43:44 +0100

    update

--- Catcher/Framework/System/Engine.js
+++ Catcher/Framework/System/Engine.js
@@ -1,10 +1,10 @@
-﻿let chosenRequestFrame;
+﻿window.onload = () => { new Images(() => this.OnImagesLoaded()); };
+
+let chosenRequestFrame;
 let spaceGame;
 let fpsLabel;
 var fps = { startTime: 0, frameNumber: 0, getFPS: function () { this.frameNumber++; var d = new Date().getTime(), currentTime = (d - this.startTime) / 1000, result = Math.floor((this.frameNumber / currentTime)); if (currentTime > 1) { this.startTime = new Date().getTime(); this.frameNumber = 0; } return result; } };
 let gamePadConnected = false;
-
-window.onload = () => { new Images(() => this.OnImagesLoaded()); };
 
 //function DetectGamePad() {
 
